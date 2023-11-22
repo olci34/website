c847b74fd95ed4a09b27da1bc9dffc0206d2670b
olci34 <developerogulcan@gmail.com>
2023-11-22 03:40:23 -0500

    VS Code gpg test

--- components/coreSphere.js
+++ components/coreSphere.js
@@ -4,7 +4,7 @@ const CoreSphere = () => {
     return (
         <mesh>
             <sphereGeometry args={[15,32,16]}/>
-            <meshStandardMaterial color="#F7FAFC" metalness={0.2} wireframe={true}/>
+            <meshStandardMaterial color="#F7FAFC" metalness={0.3} wireframe={true}/>
             <Sparkles count={50} scale={19} size={15} speed={0.5} />
         </mesh>
     )
